67d5dee0466ea6659f67a3bd412b10a7b10a3331
liwanzhen <2274036472@qq.com>
2021-07-11 10:01:53 +0800

    尝试修改

--- yyj/src/main.ts
+++ yyj/src/main.ts
@@ -7,8 +7,8 @@ import vant from './tool/vant'
 // import Button from 'vant/es/button';
 // import 'vant/es/button/style';
 const app=createApp(App)
-// app.use(Button)
 app.use(vant)
 app.use(store).use(router)
+
 
 app.mount('#app')
