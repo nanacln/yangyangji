400c9c3fcf1853d5bf0a0319fc8377c6fd33d3dc
liwanzhen <2274036472@qq.com>
2021-07-11 10:03:56 +0800

    第二次修改

--- yyj/src/main.ts
+++ yyj/src/main.ts
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import vant from './tool/vant'
 // import "./assets/font/iconfont.css";
-// import Button from 'vant/es/button';
 // import 'vant/es/button/style';
 const app=createApp(App)
 app.use(vant)
